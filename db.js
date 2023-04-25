0e52e6b13c62c61414cb5e2f7cddfd56194055ca
Arthur Lima <als.arthurlimasilva@gmail.com>
2023-04-25 17:41:50 -0300

    avanços no banco de dados

--- db.js
+++ db.js
@@ -6,15 +6,32 @@ db.serialize(function(){
         CREATE TABLE IF NOT EXISTS arquivos(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             titulo TEXT,
+            ultima_modificacao DATE,
             arquivo VARBINARY
         );
     `)
 
-    // db.all(`SELECT * FROM arquivos`, function(err, rows){
-    //     if (err) return console.log(err)
+    // const query = `INSERT INTO arquivos(
+    //     titulo,
+    //     ultima_modificacao,
+    //     arquivo
+    // ) VALUES (
+    //     SELECT 'Essay-BR', '2023-04-25', *
+    //     FROM OPENROWSET(BULK './artigos/Essay-BR a Brazilian Corpus of Essays.pdf', SINGLE_BLOB) file;
+    // );`
 
-    //     console.log(rows)
+    // db.run(query, function(err) {
+    //     console.log("INSERT")
+    //     if (err) return console.log(err)
+        
+    //     console.log(this)
     // })
+
+    db.all(`SELECT * FROM arquivos`, function(err, rows){
+        if (err) return console.log(err)
+
+        console.log(rows)
+    })
 })
 
 module.exports = db
