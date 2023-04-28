09a4a9fcbe7b4645dd63fd7d05ad606e84276f8b
Arthur Lima <als.arthurlimasilva@gmail.com>
2023-04-27 22:59:28 -0300

    organização de arquivo

--- server.js
+++ server.js
@@ -37,37 +37,6 @@ function isFileValid(file) {
     return true;
 };
 
-
-
-// CONTROLE DAS ROTAS
-server.get("/", function(req, res) {
-    return res.render("index.html")
-})
-
-server.get("/cadastro", function(req, res) {
-    return res.render("cadastro.html")
-})
-
-server.get("/esqueceu_senha", function(req, res) {
-    return res.render("esqueceu_senha.html")
-})
-
-server.post("/", function(req, res) {
-    return res.render("index.html")
-})
-
-server.post("/principal", function(req, res) {
-    fh_res = fileHandling(req, res)
-
-    const entradas = [
-        {titulo: "Documento genérico 1", ultima_modificacao: "25/04/2023"},
-        {titulo: "Documento genérico 2", ultima_modificacao: "17/02/2023"},
-        {titulo: "Documento genérico 3", ultima_modificacao: "01/01/2023"}
-    ]
-
-    return res.render("principal.html", {entradas})
-})
-
 function fileHandling(req, res) {
     const form = new formidable.IncomingForm();
     const uploadFolder = path.join(__dirname, "public", "files");
@@ -129,6 +98,37 @@ function fileHandling(req, res) {
         }
     });
 }
+
+
+
+// CONTROLE DAS ROTAS
+server.get("/", function(req, res) {
+    return res.render("index.html")
+})
+
+server.get("/cadastro", function(req, res) {
+    return res.render("cadastro.html")
+})
+
+server.get("/esqueceu_senha", function(req, res) {
+    return res.render("esqueceu_senha.html")
+})
+
+server.post("/", function(req, res) {
+    return res.render("index.html")
+})
+
+server.post("/principal", function(req, res) {
+    fh_res = fileHandling(req, res)
+
+    const entradas = [
+        {titulo: "Documento genérico 1", ultima_modificacao: "25/04/2023"},
+        {titulo: "Documento genérico 2", ultima_modificacao: "17/02/2023"},
+        {titulo: "Documento genérico 3", ultima_modificacao: "01/01/2023"}
+    ]
+
+    return res.render("principal.html", {entradas})
+})
 
 // Iniciar o servidor
 server.listen(3000)
